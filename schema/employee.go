cbd639fe3adfe26aa2b0821591f7857903ff7146
mkrouwer <Woolf@Margots-MBP.fios-router.home>
2017-10-03 03:36:17 -0400

    adding in mutations

--- schema/employee.go
+++ schema/employee.go
@@ -2,11 +2,13 @@ package schema
 
 import (
 	"github.com/graphql-go/graphql"
-	"log"
 )
 
 var (
-	EmployeeSchema graphql.Schema
+	groupDatabase = map[string]string{"lunchBunch":"LUNCH", "frisbeeCrew":"SPORTS", "chuckNorrisFanClub":"HOBBIES", "helloWorldGoodbyeSocialLife":"CODING"}
+	groups = []string{"lunchBunch", "frisbeeCrew"}
+	groupTypes = []string{"FOOD", "SPORTS"}
+	groupSuggestions = []string{}
 	groupTypesList = graphql.EnumValueConfigMap{
 		"FOOD": &graphql.EnumValueConfig{Value: "FOOD"},
 		"SPORTS": &graphql.EnumValueConfig{Value: "SPORTS"},
@@ -17,6 +19,7 @@ var (
 		Name:   "Groups",
 		Values: groupTypesList,
 	})
+
 	campusList = graphql.EnumValueConfigMap{
 		"NEW_YORK_CITY": &graphql.EnumValueConfig{Value: "NEW_YORK_CITY"},
 		"CORK": &graphql.EnumValueConfig{Value: "CORK"},
@@ -36,12 +39,7 @@ const (
 	CURRENT_CAMPUS = "NEW_YORK_CITY"
 )
 
-
-func init() {
-	groups := []string{"lunchBunch", "frisbeeCrew"}
-	groupTypes := []string{"FOOD", "SPORTS"}
-
-	employeeType := graphql.NewObject(graphql.ObjectConfig{
+	var employeeType = graphql.NewObject(graphql.ObjectConfig{
 		Name:        "Employee",
 		Description: "A company employee.",
 		Fields: graphql.Fields{
@@ -87,6 +85,13 @@ func init() {
 					return groups, nil
 				},
 			},
+			"groupSuggestions": &graphql.Field{
+				Type:     graphql.NewList(graphql.String),
+				Description: "Suggestions for groups the user might enjoy.",
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					return groupSuggestions, nil
+				},
+			},
 			"groupTypes": &graphql.Field{
 				Type:        graphql.NewList(groupTypesEnum),
 				Description: "The categories of the group a given employee is a part of.",
@@ -97,7 +102,7 @@ func init() {
 		},
 	})
 
-	queryType := graphql.NewObject(graphql.ObjectConfig{
+	var queryType = graphql.NewObject(graphql.ObjectConfig{
 		Name: "Query",
 		Fields: graphql.Fields{
 			"employees": &graphql.Field{
@@ -108,12 +113,49 @@ func init() {
 			},
 		},
 	})
-	var err error
-	EmployeeSchema, err = graphql.NewSchema(graphql.SchemaConfig{
-		Query:    queryType,
+
+	var mutationType = graphql.NewObject(graphql.ObjectConfig{
+		Name: "Mutation",
+		Fields: graphql.Fields{
+			"addGroup": &graphql.Field{
+				Type: employeeType,
+				Args: graphql.FieldConfigArgument{
+					"groupName": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.String),
+					},
+					"groupType": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.String),
+					},
+				},
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					args := make(map[string]string)
+					for k, v := range p.Args {
+						args[k] = v.(string)
+					}
+					addGroup(args)
+					return p.Source, nil
+				},
+			},
+		},
 	})
 
-	if err != nil {
-		log.Fatalf("failed to create new schema, error: %v", err)
+func addGroup(args map[string]string) {
+	groupName := args["groupName"]
+	groupType := args["groupType"]
+	groups = append(groups, groupName)
+	for _, group := range groupTypes{
+		if(group == groupType){
+			return
+		}
 	}
+	groupTypes = append(groupTypes, groupType)
 }
+
+
+
+	var EmployeeSchema, err = graphql.NewSchema(graphql.SchemaConfig{
+		Query:    queryType,
+		Mutation: mutationType,
+	})
+
+
